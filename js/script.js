22342076c16fe5569c54dee2e52a54690f1177bf
Neposyda <v.guard333@gmail.com>
2020-05-21 19:20:06 +0300

    js iframe + table

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
-function clickButton (textHref) {
+
+
+
+
+function hideBlock(nameBlock){
+	switch (nameBlock){
+		case "reserve":
+			document.querySelector("#videoframe").hidden = false;
+			document.querySelector(".boxseats").hidden = true;
+			break;
+		case "trailer":
+			document.querySelector("#videoframe iframe").setAttribute("src","");
+			document.querySelector("#videoframe").hidden = true;
+			document.querySelector(".boxseats").hidden = false;
+			break;
+		default:
+			return 0;
+	}
+}
+
+function selectTrailer (textHref) {
+	hideBlock("reserve");
 	document.querySelector("#videoframe iframe").setAttribute("src",textHref);
 }
