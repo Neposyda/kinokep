daf74457eb18374a3fd797d5569b57c7e445259e
Neposyda <v.guard333@gmail.com>
2020-05-22 19:03:32 +0300

    ended order tip

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 
-
+var colors = ["green","red","yellow"]
+var listPlaces=[];
+var places = {
+	"film1":[
+	[0,0,1,0,1,1,1,0], 
+	[0,1,1,0,1,0,1,0],
+	[0,0,1,0,1,0,1,0],
+	[0,0,1,0,1,0,1,0],
+	[0,0,1,0,1,1,1,0] ],
+	"film2":[
+	[0,0,1,0,1,0,1,0], 
+	[0,1,1,0,1,0,1,0],
+	[0,0,1,0,1,1,1,0],
+	[0,0,1,0,0,0,1,0],
+	[0,0,1,0,0,0,1,0] ],
+	"film3":[
+	[0,0,1,0,1,1,1,0], 
+	[0,1,1,0,1,0,1,0],
+	[0,0,1,0,1,1,1,0],
+	[0,0,1,0,1,0,1,0],
+	[0,0,1,0,1,1,1,0] ],
+	};
 
 
 function hideBlock(nameBlock){
@@ -18,7 +39,38 @@ function hideBlock(nameBlock){
 	}
 }
 
-function selectTrailer (textHref) {
+function clickButtonTrailer(textHref) {
 	hideBlock("reserve");
 	document.querySelector("#videoframe iframe").setAttribute("src",textHref);
+}
+
+function setColorPlace(numPlace, numColor){
+	document.getElementById(numPlace).setAttribute("class",colors[numColor]);
+}
+
+function clickButtonTip(name){
+	hideBlock("trailer");
+	document.getElementById("namefilm").innerText = document.getElementById(name).innerText;
+	let arrPlaces=places[name];
+	arrPlaces.forEach((row,i) => row.forEach((num,j) => setColorPlace(""+(i+1)+(j+1),num)));
+}
+
+
+function clickOnTable(){
+	let numPlace=event.target.id, statusPlace=event.target.className; 
+	let i=numPlace[0]-1, j=numPlace[1]-1;
+	switch (statusPlace){
+		// case "red":
+		// 	alert("На місце в залі №"+numPlace+" квиток вже придбали.");
+		// 	break;
+		case "green":
+			if (confirm("Бажаєте забронювати в залі місце №:"+numPlace)) setColorPlace(numPlace, 2);
+			listPlaces.push(numPlace);			
+			break;
+		case "yellow":
+			if (confirm("Знімаєте замовлення на місце №:"+numPlace)) setColorPlace(numPlace, 0);
+			listPlaces=listPlaces.filter(num => num!=numPlace);
+			break;	
+		}
+		document.getElementById("listplace").innerText=listPlaces.join(",");
 }
