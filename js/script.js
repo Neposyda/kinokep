16f020d5e7b8292c27cdb83dee70522776603fa9
Volodymyr Strazhnyk <if270974svm@gmail.com>
2020-05-24 21:39:59 +0300

    24 05 edit

--- js/script.js
+++ js/script.js
@@ -2,28 +2,30 @@
 var colors = ["green","red","yellow"]
 var listPlaces=[];
 var places = {
-	"film1":[
+	"film1":[[
 	[0,0,1,0,1,1,1,0], 
 	[0,1,1,0,1,0,1,0],
 	[0,0,1,0,1,0,1,0],
 	[0,0,1,0,1,0,1,0],
-	[0,0,1,0,1,1,1,0] ],
-	"film2":[
+	[0,0,1,0,1,1,1,0] ],[]],
+	"film2":[[
 	[0,0,1,0,1,0,1,0], 
 	[0,1,1,0,1,0,1,0],
 	[0,0,1,0,1,1,1,0],
 	[0,0,1,0,0,0,1,0],
-	[0,0,1,0,0,0,1,0] ],
-	"film3":[
+	[0,0,1,0,0,0,1,0] ],[]],
+	"film3":[[
 	[0,0,1,0,1,1,1,0], 
 	[0,1,1,0,1,0,1,0],
 	[0,0,1,0,1,1,1,0],
 	[0,0,1,0,1,0,1,0],
-	[0,0,1,0,1,1,1,0] ],
+	[0,0,1,0,1,1,1,0] ],[]]
 	};
 
 
 function hideBlock(nameBlock){
+	let namefilm=document.getElementById("placescinema").getAttribute("name");
+	if (namefilm!="") places[namefilm][1]=listPlaces;
 	switch (nameBlock){
 		case "reserve":
 			document.querySelector("#videoframe").hidden = false;
@@ -50,9 +52,15 @@ function setColorPlace(numPlace, numColor){
 
 function clickButtonTip(name){
 	hideBlock("trailer");
-	document.getElementById("namefilm").innerText = document.getElementById(name).innerText;
-	let arrPlaces=places[name];
+	document.getElementById("namefilm").innerText = document.getElementById(name).innerText;	
+	document.getElementById("placescinema").setAttribute("name", name);
+	let arrPlaces=places[name][0];
 	arrPlaces.forEach((row,i) => row.forEach((num,j) => setColorPlace(""+(i+1)+(j+1),num)));
+	listPlaces=places[name][1];
+	if (listPlaces.length) {
+		listPlaces.forEach(el => setColorPlace(el,2));
+		document.getElementById("listplace").innerText=listPlaces.join(",");
+	} else {document.getElementById("listplace").innerText = "...";}
 }
 
 
@@ -64,13 +72,16 @@ function clickOnTable(){
 		// 	alert("На місце в залі №"+numPlace+" квиток вже придбали.");
 		// 	break;
 		case "green":
-			if (confirm("Бажаєте забронювати в залі місце №:"+numPlace)) setColorPlace(numPlace, 2);
-			listPlaces.push(numPlace);			
+			if (confirm("Бажаєте забронювати в залі місце №:"+numPlace)) {
+				setColorPlace(numPlace, 2);
+				listPlaces.push(numPlace);
+			}
 			break;
 		case "yellow":
-			if (confirm("Знімаєте замовлення на місце №:"+numPlace)) setColorPlace(numPlace, 0);
-			listPlaces=listPlaces.filter(num => num!=numPlace);
-			break;	
+			if (confirm("Знімаєте замовлення на місце №:"+numPlace)) {
+				setColorPlace(numPlace, 0);
+				listPlaces=listPlaces.filter(num => num!=numPlace);
+			}
 		}
 		document.getElementById("listplace").innerText=listPlaces.join(",");
 }
